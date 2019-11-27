0914fb3a638536a60dd5c2bfd21bbf759b06a8d9
Matheus Graca <matheus.graca@gmail.com>
2019-11-27 09:05:43 -0300

    Adicionar tela de conversao de imagem

--- app/.aws-sam/build/ErrorFunction/html-response.js
+++ app/.aws-sam/build/ErrorFunction/html-response.js
@@ -1,26 +1,10 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 module.exports = function htmlResponse(body) {
-	return {
-		statusCode:200,
-		body: body,
-		headers: {
-			'Content-Type': 'text/html'
-		}
-	}
+  return {
+    statusCode: 200,
+    body: body,
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }
+};
 
-}
--- app/.aws-sam/build/ErrorFunction/show-form.js
+++ app/.aws-sam/build/ErrorFunction/show-form.js
@@ -1,87 +1,27 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandlerOld = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Hello world 10 porcent',
-                // location: ret.data.trim()
-            })
-        }
-	throw new Error('listId does not exist');
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
-    return response
-};
-
-exports.lambdaError = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'error',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
-    return response
-};
-
 const htmlResponse = require('./html-response');
-const formHtml = `
-	<html>
-	<head>
-		<meta charset="utf-8"/>
-	</head>
-	<body>
-		<form method="POST">
-			Please enter your name:
-			<input type="text" name="acao" />
-			<input type="submit" />
-		</form>
-	<body>
-	</html>
-`;
+const buildForm = require('./build-form');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+const uploadLimitInMB = parseInt(process.env.UPLOAD_LIMIT_IN_MB);
 
-const thanksHtml = `
-	<html>
-	<head>
-		<meta charset="utf-8"/>
-	</head>
-	<body>
-		<h1>Thanks</h1>
-		<p> We Received submission</p>
-	</body>
-	</html>
-`;
+exports.lambdaHandler = async (event, context) => {
 
+  const apiHost = event.requestContext.domainName,
+    prefix = event.requestContext.stage,
+    redirectUrl = `https://${apiHost}/${prefix}/confirm`,
 
-exports.lambdaHandler = async(event, context) => {
-
-	console.log(JSON.stringify(event, null, 2));
-	return htmlResponse(formHtml);
+    params = {
+      Bucket: process.env.UPLOAD_S3_BUCKET,
+      Expires: 600,
+      Conditions: [
+        ['content-length-range', 1, uploadLimitInMB * 1000000],
+      ],
+      Fields: {
+        success_action_redirect: redirectUrl,
+        acl: 'private',
+        key: context.awsRequestId + '.jpg'
+      }
+    },
+    form = s3.createPresignedPost(params);
+  return htmlResponse(buildForm(form));
 };
