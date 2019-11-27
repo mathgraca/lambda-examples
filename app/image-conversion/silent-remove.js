3dc1b7d0b4dd2044d744dfff2a9355b3b7c1100b
Matheus Graca <matheus.graca@gmail.com>
2019-11-27 09:09:20 -0300

    Adicionar tela de conversao de imagem

---
+++ app/image-conversion/silent-remove.js
@@ -0,0 +1,12 @@
+const util = require('util'),
+	fs = require('fs'),
+	removeAsync = util.promisify(fs.unlink);
+
+module.exports = async function silentRemove(file) {
+
+	try {
+		await removeAsync(file);
+	} catch (e) {
+		console.error('Problema ao deletar arquivo');
+	}
+};
